6cf27f6c457ba788f6c6c04c8350d5a969c30e00
Cai Silva <crs_caique@hotmail.com>
2026-04-23 11:03:44 +1200

    Page name

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,9 @@ import { About } from "./components/About";
 import { TechnologiesTools } from "./components/TechnologiesTools";
 import "./index.css";
 
-const Header = () => {
-  return (
-    <title>
-      Portfolio - Caique R. Silva
-    </title>
-  )
-}
 function App() {
   return (
     <div id="app-scroll-root" className="min-h-screen overflow-x-hidden overflow-y-auto">
-      <Header/>
       <Homepage />
       <About />
       <TechnologiesTools />
